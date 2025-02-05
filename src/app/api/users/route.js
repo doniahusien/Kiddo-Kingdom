008ff18fff610c7ee7e792c0f21471f78e391681
doniahusien <doniahusien@gmail.com>
2025-02-05 16:14:58 +0200

    feat: implement api for auth

---
+++ src/app/api/users/route.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+import path from "path";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { email, password } = await request.json();
+    const filePath = path.join(process.cwd(), "public/users.json");
+
+    // Check if the file exists
+    if (!fs.existsSync(filePath)) {
+      return NextResponse.json({ error: "users.json not found" }, { status: 404 });
+    }
+
+    const fileData = fs.readFileSync(filePath, "utf-8");
+    const users = JSON.parse(fileData);
+
+    // Find the user with matching credentials
+    const user = users.find((user) => user.email === email && user.password === password);
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+    }
+
+    return NextResponse.json(user); // Return user data including password
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
