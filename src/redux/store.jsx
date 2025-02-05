5a4624e01ee8b840bcf629e12c7cd4100e1c96c5
doniahusien <doniahusien@gmail.com>
2025-02-06 00:01:37 +0200

    refactor: update store with product reducer

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/authSlice'
+import productsReducer from './features/productSlice'
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        
+        products:productsReducer,
     }
 })
 export default store;
