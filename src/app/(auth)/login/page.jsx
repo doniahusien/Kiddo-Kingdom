7383d8f96807d5c8b2b4fc1a066f6d30f45afb3b
doniahusien <doniahusien@gmail.com>
2025-02-05 16:13:16 +0200

    feat: create auth pages

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginForm from '@/components/LoginForm'
+const loginPage = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default loginPage
---
+++ src/app/(auth)/signup/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+import SignUpForm from '@/components/SignUpForm'
+const signupPage = () => {
+
+  return (
+    <div><SignUpForm /></div>
+  )
+}
+
+export default signupPage
